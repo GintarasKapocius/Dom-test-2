53257d79c57174fe2a25695305d88acdd6e3d2b7
GintarasKapocius <gintaraskapocius@gmail.com>
2021-11-14 19:02:45 +0200

    task 6

--- js/components/ToysGridComponent.js
+++ js/components/ToysGridComponent.js
@@ -1,9 +1,32 @@
 class ToysGridComponent {
   constructor() {
+    this.state = { loading: false, toys: [] }
     this.init()
   }
+
+  fetchToys = () =>
+    setInterval(() => API.fetchToys(this.saveToysData, alert), 1000)
+
+  saveToysData = (toys) => {
+    this.state.toys = toys
+    this.state.loading = false
+    this.render()
+  }
+
   init = () => {
+    this.state.loading = true
+    this.fetchToys()
     this.htmlElement = document.createElement('div')
-    this.htmlElement.innerHTML = 'Toys Grid Component'
+    //task 4 this.htmlElement.innerHTML = 'Toys Grid Component'
+    this.render()
+  }
+
+  render = () => {
+    const { loading, toys } = this.state
+    if (loading) {
+      this.htmlElement.innerHTML = 'Siunciama'
+    } else {
+      this.htmlElement.innerHTML = 'Parsiusta'
+    }
   }
 }
